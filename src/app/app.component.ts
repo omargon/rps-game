8b79b4f9d661cc21cac27fb5ee4b0f462ee421c0
Omar Gonzalez <omargon@gmail.com>
2023-01-31 10:50:52 -0500

    Feature: Players Round Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,17 @@ enum GameStates {
   EndOfRound = 'End Of Round', 
 }
 
+enum PlayOptions {
+  Rock = 'Rock',
+  Paper = 'Paper',
+  Scissors = 'Scissors',
+}
+
+enum Players {
+  PlayerOne = 'Player 1',
+  PlayerTwo = 'Player 2',
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,12 +32,19 @@ enum GameStates {
 export class AppComponent {
   GameTypes = GameTypes;
   GameStates = GameStates;
-  
+  PlayOptions = PlayOptions;
+  Players = Players;
+
   title = 'RPS Game';
   gameType = GameTypes.OnePlayer;
   gameState = GameStates.New;
   playerOneName = 'Player One';
-  playerTwoName = 'Player Two'
+  playerTwoName = 'Player Two';
+  currentPlayer = Players.PlayerOne;
+  hands = {
+    playerOne: PlayOptions.Rock,
+    playerTwo: PlayOptions.Rock,
+  };
 
   selectGameType(gameTypeSelected: GameTypes) {
     this.gameType = gameTypeSelected;
@@ -35,5 +53,24 @@ export class AppComponent {
 
   play(){
     this.gameState = GameStates.Player1Turn;
+  }
+
+  submitPlayerHand(playOption: PlayOptions ) {
+    if(this.gameState == GameStates.Player1Turn) {
+      this.hands.playerOne = playOption;
+      this.gameState = GameStates.Player2Turn;
+      if (this.gameType == GameTypes.OnePlayer) {
+        this.submitComputersHand();
+      }
+    } else {
+      this.hands.playerTwo = playOption;
+      this.gameState = GameStates.EndOfRound;
+    }
+  }
+
+  submitComputersHand() {
+    const computerChoices = Object.values(PlayOptions);
+    const randomIndex = Math.floor(Math.random() * 3);
+    this.submitPlayerHand(computerChoices[randomIndex]);
   }
 }
