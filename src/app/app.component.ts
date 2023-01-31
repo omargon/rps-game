952970ea0c660236db58bb33fdfc649ae05d381c
Omar Gonzalez <omargon@gmail.com>
2023-01-31 10:07:37 -0500

    Feature: Player Names Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
 
+enum GameTypes {
+  OnePlayer = '1 Player',
+  TwoPlayers = '2 Players',
+}
+
+enum GameStates {
+  New = 'New',
+  Started = 'Started',
+  Player1Turn = 'Player 1 turn',
+  Player2Turn = 'Player 2 turn',
+  EndOfRound = 'End Of Round', 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
+  GameTypes = GameTypes;
+  GameStates = GameStates;
+  
   title = 'RPS Game';
-  gameType = 0;
-  gameTypes = ['1 Player', '2 Players'];
+  gameType = GameTypes.OnePlayer;
+  gameState = GameStates.New;
+  playerOneName = 'Player One';
+  playerTwoName = 'Player Two'
 
-  selectGameType(gameTypeSelected: number) {
+  selectGameType(gameTypeSelected: GameTypes) {
     this.gameType = gameTypeSelected;
+    this.gameState = GameStates.Started;
+  }
+
+  play(){
+    this.gameState = GameStates.Player1Turn;
   }
 }
